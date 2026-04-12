014bab72c501f8c3bae1a2906d99737c35cfbf5c
Ahmed Talal <ahmed.talal.egy@gmail.com>
2026-04-12 23:20:27 +0200

    feat: implement profile page components

--- src/components/layout/app/app-layout.tsx
+++ src/components/layout/app/app-layout.tsx
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 export default function AppLayout() {
   return (
     <div className="flex flex-col min-h-screen">
-      <main>
+      <main className="flex-1">
         <Outlet />
       </main>
       <Footer />
