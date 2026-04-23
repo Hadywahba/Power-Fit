9cb3481865bd95206e74a7e22dea1cee4e667980
Mostafa3lyan <96500374+Mostafa3lyan@users.noreply.github.com>
2026-04-23 03:42:22 +0200

    chore: remove duplicated code

--- src/pages/app/home/page.tsx
+++ src/pages/app/home/page.tsx
@@ -1,13 +1,15 @@
 import AboutPage from '../about/page';
 import ClassesPage from '../classes/page';
 import HealthyPage from '../healthy/page';
+import HeroSection from './components/hero-section';
 
 export default function HomePage() {
   return (
-    <main className='relative grid w-full grid-cols-1 '>
-     <div className='pt-8'>
-       <ClassesPage />
-     </div>
+    <main className="relative grid w-full grid-cols-1">
+      <HeroSection />
+      <div className="pt-8">
+        <ClassesPage />
+      </div>
 
       <AboutPage />
 
