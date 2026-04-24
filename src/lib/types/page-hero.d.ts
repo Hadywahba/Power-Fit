1a76e3048c8dd1cc261effb81f1b17201853ad8b
Mostafa3lyan <96500374+Mostafa3lyan@users.noreply.github.com>
2026-04-24 06:00:43 +0300

    chore: resolve build errors

--- src/lib/types/page-hero.d.ts
+++ src/lib/types/page-hero.d.ts
@@ -1,7 +1,7 @@
 import type { ReactNode } from 'react';
 
 export type PageHeroProps = {
-  badge: string;
+  badge?: string;
   titleStart?: string;
   titleHighlight?: string;
   titleEnd?: string;
