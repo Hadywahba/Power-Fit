bafceb5b4d4a34bfc9818c436ce52be227d78a47
Mostafa3lyan <96500374+Mostafa3lyan@users.noreply.github.com>
2026-05-02 07:39:07 +0300

    refactor login page to match ui

--- src/pages/auth/login/components/login-form.tsx
+++ src/pages/auth/login/components/login-form.tsx
@@ -1,17 +1,16 @@
-import { useForm, type SubmitHandler } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useTranslations } from 'use-intl';
-import { Link } from 'react-router-dom';
+import SocialLogin from '@/components/shared/social-icons';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { ROUTES } from '@/lib/constants/routes/routes.constant';
 import {
   useLoginSchema,
   type LoginFields,
 } from '@/lib/schemes/auth/login.schema';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
-import { ROUTES } from '@/lib/constants/routes/routes.constant';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm, type SubmitHandler } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import { useTranslations } from 'use-intl';
 import { useLogin } from '../hooks/use-login';
-import { Apple, Chrome, Facebook } from 'lucide-react';
-import SocialLogin from '@/components/shared/social-icons';
 
 // Default Values
 const defaultValues: LoginFields = {
@@ -50,12 +49,9 @@ export default function LoginForm() {
           {t('welcome-back')}!
         </h1>
       </div>
-     <div className="w-full max-w-96 rounded-xl border border-gray-300 p-8 shadow-2xl backdrop-blur-md md:p-10">
-        <h2 className=" text-center text-2xl font-bold">{t('login')}</h2>
-        <form
-          onSubmit={handleSubmit(handleLogin)}
-          className="space-y-4  py-4"
-        >
+      <div className="w-full max-w-96 rounded-[3.125rem] border border-gray-50/20 p-8 shadow-2xl backdrop-blur-md md:p-10">
+        <h2 className="text-center text-2xl font-bold">{t('login')}</h2>
+        <form onSubmit={handleSubmit(handleLogin)} className="space-y-4 py-4">
           {/* Email */}
           <Input
             {...register('email')}
@@ -63,7 +59,7 @@ export default function LoginForm() {
             type="email"
             error={errors.email?.message}
             placeholder={t('email-placeholder')}
-            className="h-12 rounded-full border-gray-300 bg-transparent pl-12 focus-visible:border-orange-600 focus-visible:ring-orange-600"
+            className="h-12 border-gray-300 bg-transparent pr-12 focus-visible:border-orange-600 focus-visible:ring-orange-600"
           />
 
           {/* Password */}
@@ -73,7 +69,7 @@ export default function LoginForm() {
             type="password"
             error={errors.password?.message}
             placeholder={t('password-placeholder')}
-            className="h-12 rounded-full border-gray-300 bg-transparent pl-12 focus-visible:border-orange-600 focus-visible:ring-orange-600"
+            className="h-12 border-gray-300 bg-transparent pr-12 focus-visible:border-orange-600 focus-visible:ring-orange-600"
           />
 
           {/* Forget Password */}
@@ -85,33 +81,7 @@ export default function LoginForm() {
               {t('forgot-your-password')}
             </Link>
           </div>
-          {/* devider */}
-         {/* <div className="relative flex items-center justify-center">
-  <span className="relative px-3 text-xs text-gray-500 italic
-    before:content-[''] before:absolute before:left-0 before:top-1/2 before:-translate-y-1/2 before:-translate-x-full before:h-1 before:w-25 before:bg-gray-500
-    after:content-[''] after:absolute after:right-0 after:top-1/2 after:-translate-y-1/2 after:translate-x-full after:h-1 after:w-25 after:bg-gray-500
-  ">
-    {t('or')}
-  </span>
-</div> */}
 
-<SocialLogin/>
-
-          {/* Social Icons Placeholder */}
-          <div className="flex justify-center gap-5 pb-2">
-            <div className="flex h-9 w-28 cursor-pointer items-center justify-around rounded-full   transition-colors ">
-              <div className="h-6 w-6 flex items-center justify-center rounded-full  opacity-70 bg-zinc-800">
-                <Facebook size={16} className="text-gray-100" />
-             
-        </div>
-                <div className="h-6 w-6 flex items-center justify-center rounded-full  opacity-70 bg-zinc-800">
-                  <Chrome size={16} className="text-gray-100" />
-                </div>
-                <div className="h-6 w-6 flex items-center justify-center rounded-full  opacity-70 bg-zinc-800">
-                  <Apple size={16} className="text-gray-100" />
-                </div>
-            </div>
-          </div>
           {/* Submit */}
           <Button
             type="submit"
@@ -123,24 +93,23 @@ export default function LoginForm() {
             {t('login')}
           </Button>
         </form>
-          <div className="mt-3 flex justify-center text-sm">
-        <span>
-          {t('dont-have-account')}
-          <Link
-            to={ROUTES.auth.register}
-            className="text-main ms-1 font-semibold underline"
-          >
-            {t('register')}
-          </Link>
-        </span>
-      </div>
+
+        <SocialLogin />
+
+        <div className="mt-3 flex justify-center text-sm">
+          <span>
+            {t('dont-have-account')}
+            <Link
+              to={ROUTES.auth.register}
+              className="text-main ms-1 font-semibold underline"
+            >
+              {t('register')}
+            </Link>
+          </span>
+        </div>
       </div>
       {/* Navigate to Register */}
-
     </section>
-          // <SmartCoachChat />
-
-          
-
+    // <SmartCoachChat />
   );
 }
